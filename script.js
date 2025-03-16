f465e7f5c8b06bf127a13be3645b63a55bc382f7
schulya <scherbinintoscha@yandex.ru>
2025-03-16 20:37:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// script.js
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+
+    // Элемент, который будет двигаться
+    var mainContent = document.querySelector('.first_page_main_content');
+    var header = document.querySelector('.header');
+
+    // Скорость параллакса для всего блока
+    var parallaxSpeed = 0.5; // Чем меньше, тем медленнее будет движение
+    var parallaxSpeedHeader = 0.8;
+
+    // Применяем эффект параллакса только по оси Y
+    mainContent.style.transform = 'translateY(calc(' + scrollPosition * parallaxSpeed + 'px))';
+    header.style.transform = 'translateY(calc(' + scrollPosition * parallaxSpeedHeader + 'px))';
+});
