0a114e1f43dbadf4053fc6a93eb253aa7f4c2d32
schulya <scherbinintoscha@yandex.ru>
2025-03-19 21:32:06 +0500

    Create globe3.js

---
+++ globe3.js
@@ -0,0 +1,14 @@
+VANTA.GLOBE({
+    el: "#globe",
+    mouseControls: true,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 0.80,
+    scaleMobile: 0.80,
+    backgroundColor: 0x171717, 
+    color: 0xD9D9D9,
+    color2: 0xF6EFBA,
+    size: 0.80
+  })
