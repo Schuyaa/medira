fa59f7077eb389632f80855b2fef42ff2843e5b5
schulya <scherbinintoscha@yandex.ru>
2025-03-25 00:11:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+let lastScrollY = 0;
+let ticking = false;
+
 window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            var scrollPosition = window.scrollY;
 
-    var mainContent = document.querySelector('.first_page_main_content');
-    var header = document.querySelector('.header');
+            var mainContent = document.querySelector('.first_page_main_content');
+            var header = document.querySelector('.header');
 
-    var parallaxSpeed = 0.5; // чем меньше, тем медленнее 
-    var parallaxSpeedHeader = 0.8;
+            var parallaxSpeed = 0.5; // чем меньше, тем медленнее 
+            var parallaxSpeedHeader = 0.8;
 
-    mainContent.style.transform = 'translateY(calc(' + scrollPosition * parallaxSpeed + 'px))';
-    header.style.transform = 'translateY(calc(' + scrollPosition * parallaxSpeedHeader + 'px))';
+            mainContent.style.transform = 'translateY(' + (scrollPosition * parallaxSpeed) + 'px)';
+            header.style.transform = 'translateY(' + (scrollPosition * parallaxSpeedHeader) + 'px)';
+            
+            ticking = false;
+        });
+
+        ticking = true;
+    }
 });
